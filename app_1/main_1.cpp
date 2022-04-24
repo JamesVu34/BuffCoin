cbc8d5d4996f584ff3b6a252a5d5673c945db763
James Vu <jamesvu@Jamess-MacBook-Air.local>
2022-04-23 20:13:41 -0600

    Started main_1.cpp program and made some changes to Blockchain.cpp

--- app_1/main_1.cpp
+++ app_1/main_1.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    Blockchain buffCUoin;
+    // Blockchain buffCUoin;
 
     // buffCUoin.prettyPrint(); 
-
     // cout << "ashutosh mining first block ... " << endl;
     // buffCUoin.minePendingTransactions("ashutosh");
     // buffCUoin.prettyPrint(); 
     
-    // cout << "ashutosh paid maciej 1 BFC ... " << endl;
-    // buffCUoin.addTransaction("ashutosh","maciej", 1);
+    // cout << "ashutosh paid maciej 2 BFC ... " << endl;
+    // buffCUoin.addTransaction("ashutosh","maciej", 2);
+    // buffCUoin.prettyPrint();
 
     // cout << "ashutosh paid asa 1 BFC ... " << endl;
     // buffCUoin.addTransaction("ashutosh","asa", 1);
@@ -35,20 +35,97 @@ int main(int argc, char* argv[])
     // cout << "ashutosh mining fourth block ... " << endl;
     // buffCUoin.minePendingTransactions("ashutosh");
     // buffCUoin.prettyPrint();
-    
-    Transaction yo("james", "1234abc", 200);
-    // Transaction yo2("chris", "346def", 300);
-    vector<Transaction> trans;
-    trans.push_back(yo);
-    // trans.push_back(yo2);
-    time_t currTime;
 
-    time(&currTime);
+    /* PROGRAM STARTS HERE */
+    // variables
+    string name = "";
+    bool quit = false;
+    int option = 0;
+    string input = "";
+    Blockchain buffCUoin;
 
-    Block b(trans, currTime, "abc");
+    // ask user for name and have them input
+    cout << "Please enter your name to start: " << endl;
+    getline(cin, name);
 
-    cout << b.toString() << endl;;
-    cout << b.calculateHash();
+    cout << "Hello, " << name << "!" << endl;
+
+    while (!quit) {
+        // menu
+        cout << "=====MAIN MENU=====" << endl;
+        cout << "1. Add a Transaction" << endl;
+        cout << "2. Mine a Pending Transaction" << endl;
+        cout << "3. View Transactions" << endl;
+        cout << "4. View Transaction Information" << endl;
+        cout << "5. Quit" << endl;
+
+        // get input
+        getline(cin, input);
+        option = stoi(input);
+
+        // use switch 
+        switch (option) {
+            case 1: // add transaction
+            {
+                string sender, receiver, coins = "";
+                int amount = 0;
+
+                // ask and have user input sender, receiver, and amount
+                cout << "Please enter the sender address: " << endl;
+                getline(cin, sender);
+                cout << "Please enter the receiver address: " << endl;
+                getline(cin, receiver);
+                cout << "Please enter the amount you wish to send: " << endl;
+                getline(cin, coins);
+                amount = stoi(coins);
+
+                buffCUoin.addTransaction(sender, receiver, amount);
+
+                break;
+            }
+
+            case 2: // mine transaction
+            {
+                string address = "";
+                
+                cout << "Please enter address that you wish to mine from: " << endl;
+                getline(cin, address);
+                
+                if (buffCUoin.minePendingTransactions(address) == false) {
+                    cout << "Chain is not valid." << endl;
+                }
+                else {
+                    cout << "Mined Successfully!" << endl;
+                    cout << "Balance of " << address << ": " << buffCUoin.getBalanceOfAddress(address) << endl; 
+                }
+
+                break;
+            }
+            case 3: // view transactions
+            {
+                buffCUoin.prettyPrint();
+                break;
+            }
+
+            case 4: // view transaction info
+            {
+                break;
+            }
+
+            case 5: // quit
+            {
+                cout << "Goodbye!" << endl;
+                quit = true;
+                break;
+            }
+
+            default: 
+            {
+                cout << "Invalid option. Please enter a number 1-5." << endl;
+                break;
+            }
+        }
+    }
 
     return 0;
 }
--- code_1/Blockchain.cpp
+++ code_1/Blockchain.cpp
@@ -27,27 +27,97 @@ Block Blockchain::getLatestBlock() {
 }
 
 void Blockchain::addTransaction(string src, string dst, int coins) {
-    // TODO
+    // if it is BFC then allow transaction
+    if (src == "BFC") {
+        Transaction newTrans = Transaction(src, dst, coins);
+        pending.push_back(newTrans);
+    }
+    // if it is from one user to another check balance of src
+    else if (getBalanceOfAddress(src) > coins) {
+        Transaction newTrans = Transaction(src, dst, coins);
+        pending.push_back(newTrans);
+        cout << src << " has sent " << dst << " " << coins << " buffCuoins." << endl;
+    }
+    else {
+        cout << "Error to proccess transaction. Not enough coins in account." << endl;
+        cout << "Balance: " << getBalanceOfAddress(src) << endl;
+        cout << "Please mine pending transactions to receive coins." << endl;
+    }
 }
 
 bool Blockchain::isChainValid() { 
-    // TODO
+    // traverse block chain from second block
+    for (int i = 1; i < chain.size(); i++) {
+        string previous = chain[i-1].calculateHash();
+        string myHash = "";
+        string zeros = "";
+
+        // mine block to get nonce values for each 
+        chain[i].mineBlock(difficulty);
+        
+        // checking if previous hash matches previousHash of current Block
+        if (chain[i].calculateHash() != previous) {
+            return false;
+        }
+
+        myHash = chain[i].calculateHash();
+
+        // creating a zeros string
+        for (int j = 0; j < difficulty; j++) {
+            zeros += "0";
+        }
+
+        // if zeros string doesnt match hash then return false
+        if (myHash.substr(0, difficulty) != zeros) {
+            return false;
+        }
+
+    }
     return true;
 }
 
 bool Blockchain::minePendingTransactions(string minerAddress) {
-    // TODO
+    vector<Transaction> pend = pending;
+    // creating new Block
+    Block newBlock(pend, time(NULL), getLatestBlock().calculateHash());
+
+    newBlock.mineBlock(difficulty);
+
+    if (isChainValid() == false) {
+        return false;
+    }
+
+    // clearing transaction vector and pushing back new block onto chain
+    pend.clear();
+    chain.push_back(newBlock);
+
+    addTransaction("BFC", minerAddress, miningReward);
+
     return true;
 }
 
 int Blockchain::getBalanceOfAddress(string address) {
-    // traverse chain and find block w/ address
-    for (int i = 0; i < chain.size(); i++) {
-        
+    int paid = 0;
+    int receive = 0;
+
+    // traverse transaction vector to find address
+    for (int i = 0; i < pending.size(); i++) {
+        if (pending[i].getSender() == address) {
+            paid += pending[i].getAmount();
+        }
+        if (pending[i].getReceiver() == address) {
+            receive += pending[i].getAmount();
+        }
     }
-    return 100;
+
+    return receive - paid;
 }
 
 void Blockchain::prettyPrint() {
-    // TODO
+    cout << "Format of Print: (Sender, Receiver, Amount)" << endl;
+    // for (int i = 0; i < chain.size(); i++) {
+        for (int j = 0; j < pending.size(); j++) {
+            cout << pending[j].toString() << endl;
+        }
+    // }
 }
